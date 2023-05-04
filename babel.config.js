a150c0e67cd9da1b634183b4e45e70530a05077f
Daniil Borysov <danofu13@gmail.com>
2023-05-04 17:42:26 +0200

    Setup environment

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
+    plugins: [
+      [
+        'module-resolver',
+        { alias: { '@Assets': './assets', '@Components': './src/components' }, extensions: ['.ts', '.tsx'] },
+      ],
+    ],
     presets: ['babel-preset-expo'],
   };
 };
