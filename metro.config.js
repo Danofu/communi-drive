ddcb6b340a63e8ac7afe1667d7f5f6e6087d9ede
Daniil Borysov <danofu13@gmail.com>
2023-05-06 19:34:52 +0200

    Add firebase sdk dependency

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.assetExts.push('cjs');
+
+module.exports = defaultConfig;
