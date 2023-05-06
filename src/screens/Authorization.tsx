c8d5940d8d23dcc0dec06c191c39641a1ad4c88f
Daniil Borysov <danofu13@gmail.com>
2023-05-06 19:19:34 +0200

    Style Authorization screen

--- src/screens/Authorization.tsx
+++ src/screens/Authorization.tsx
@@ -1,10 +1,41 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
+
+import SignInForm from '@Components/SignInForm';
+import elevation from '@Util/elevation';
 
 export default function Authorization() {
   return (
-    <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
-      <Text variant="headlineMedium">Authorization</Text>
+    <View style={styles.root}>
+      <View style={styles.formContainer}>
+        <Text style={styles.headerText} variant="headlineMedium">
+          Sign In
+        </Text>
+        <SignInForm />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  formContainer: {
+    ...elevation(3),
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 8,
+    justifyContent: 'center',
+    padding: 16,
+    width: '100%',
+  },
+  headerText: {
+    marginVertical: 16,
+    textAlign: 'center',
+  },
+  root: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+  },
+});
