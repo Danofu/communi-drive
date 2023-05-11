bca9e7d8cc8cbea7b02d412bab3a764f006629b2
Daniil Borysov <danofu13@gmail.com>
2023-05-11 20:48:43 +0200

    Fix bug with forwarding ref

--- src/components/SignInForm/TextInputWithHelperText.tsx
+++ src/components/SignInForm/TextInputWithHelperText.tsx
@@ -1,16 +1,13 @@
-import { ReactElement, ReactNode } from 'react';
-import { View } from 'react-native';
+import { PropsWithRef, ReactElement, ReactNode, forwardRef } from 'react';
+import { TextInput as RNTextInput, View } from 'react-native';
 import { HelperText, TextInput, TextInputProps } from 'react-native-paper';
 
-type Props = TextInputProps & { helperText?: ReactNode; helperTextVisible?: boolean; required?: boolean };
+type Props = PropsWithRef<TextInputProps & { helperText?: ReactNode; helperTextVisible?: boolean; required?: boolean }>;
 
-export default function TextInputWithHelperText({
-  helperText,
-  helperTextVisible = false,
-  label,
-  required = false,
-  ...props
-}: Props) {
+export default forwardRef<RNTextInput, Props>(function TextInputWithHelperText(
+  { helperText, helperTextVisible = false, label, required = false, ...props },
+  ref
+) {
   const labelWithRequiredIndicator: ReactElement = (
     <>
       {label}
@@ -20,8 +17,8 @@ export default function TextInputWithHelperText({
 
   return (
     <View>
-      <TextInput label={labelWithRequiredIndicator} {...props} />
+      <TextInput label={labelWithRequiredIndicator} ref={ref} {...props} />
       {helperTextVisible && <HelperText type="error">{helperText}</HelperText>}
     </View>
   );
-}
+});
